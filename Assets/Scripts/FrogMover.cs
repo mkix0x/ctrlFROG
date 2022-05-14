1fab210b7ce6c9edc9e5b213ac02fb6d7bcdbe0a
mki <102934436+mkix0x@users.noreply.github.com>
2022-05-14 13:02:33 +0100

    .

---
+++ Assets/Scripts/FrogMover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+class FrogMover : MonoBehaviour, IMover
+{
+    [SerializeField]
+    private float groundSpeed = 4;
+    
+    [SerializeField]
+    private float airSpeed = 6;
+    
+    private Rigidbody body;
+    private GroundDetector groundDetector;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        groundDetector = GetComponent<GroundDetector>();
+    }
+    
+    public void Move(Vector3 direction)
+    {
+        float moveSpeed = groundDetector.IsGrounded ? groundSpeed : airSpeed;
+        Vector3 velocity = direction.normalized * moveSpeed;
+        body.velocity = new Vector3(velocity.x, body.velocity.y, velocity.z);
+    }
+}
